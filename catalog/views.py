aab65312f96bbd4ea83d23e24320d190e27cd17f
zlatan1487 <vitalik.karpukhin@list.ru>
2023-07-20 15:57:11 +0200

    templates fixed

--- catalog/views.py
+++ catalog/views.py
@@ -5,27 +5,10 @@ from catalog.models import Category, Product
 
 def index(request):
     context = {
-        'object_list': Category.objects.all()[:3],
-        'title': 'Главная'
+        'object_list': Product.objects.all(),
+        'title': 'Продукты'
     }
     return render(request, 'catalog/index.html', context)
-
-
-def categories(request):
-    context = {
-        'object_list': Category.objects.all(),
-        'title': 'Все категории'
-    }
-    return render(request, 'catalog/categories.html', context)
-
-
-def category_products(request, pk):
-    category_item = Category.objects.get(pk=pk)
-    context = {
-        'object_list': Product.objects.filter(category_id=pk),
-        'title': f'товары из категории {category_item.title}'
-    }
-    return render(request, 'catalog/product.html', context)
 
 
 def contacts(request):
